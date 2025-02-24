d596ebdae530cf5033ba61c43ee38cd7257c2a12
Kris <87007257+kris04090513@users.noreply.github.com>
2025-02-24 17:22:00 +0800

    修改按鈕及全域顏色變數

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TodoList from "./components/TodoList"
 export default function Home() {
   return (
    <main>
-      <h1>待辦事項清單</h1>
+      {/* <h1>待辦事項清單</h1> */}
       <TodoList />
     </main>
   );
